882d1747d3e3dc31ffc71153454f19f6f8698c28
di-saldana <dsl42@alu.ua.es>
2024-01-20 00:26:51 +0100

    list users

--- app/src/main/java/es/ua/eps/sqliteapp/ListUsers.kt
+++ app/src/main/java/es/ua/eps/sqliteapp/ListUsers.kt
@@ -2,16 +2,31 @@ package es.ua.eps.sqliteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.room.Room
+import androidx.room.RoomDatabase
 import es.ua.eps.sqliteapp.databinding.ActivityListUsersBinding
 
 class ListUsers : AppCompatActivity() {
     private lateinit var bindings: ActivityListUsersBinding
+
+    private var adapter:UserListAdapter ?= null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindings = ActivityListUsersBinding.inflate(layoutInflater)
 
         with(bindings) {
             setContentView(root)
+
+            recyclerView.layoutManager = LinearLayoutManager(this@ListUsers)
+            adapter = UserListAdapter()
+            recyclerView.adapter = adapter
+
+            val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "db.db")
+                .allowMainThreadQueries().setJournalMode(RoomDatabase.JournalMode.TRUNCATE).build()
+            val userList = db.userDAO().loadAll()
+            adapter?.addItems(userList)
 
             closeButton.setOnClickListener {
                 finish()
---
+++ app/src/main/java/es/ua/eps/sqliteapp/UserListAdapter.kt
@@ -0,0 +1,42 @@
+package es.ua.eps.sqliteapp
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class UserListAdapter : RecyclerView.Adapter<UserListAdapter.UserViewHolder>() {
+    private var users: List<User> = ArrayList()
+
+    fun addItems(items: List<User>){
+        this.users = items
+        notifyDataSetChanged()
+    }
+
+    class UserViewHolder(view: View): RecyclerView.ViewHolder(view){
+        private var username = view.findViewById<TextView>(R.id.username)
+        private var nombre = view.findViewById<TextView>(R.id.email)
+
+        fun bindView(user : User){
+            username.text = user.username
+            nombre.text = user.nombre
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = UserViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.user_item, parent, false)
+    )
+
+    override fun getItemCount(): Int {
+        return users.size
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = users[position]
+        holder.bindView(user)
+    }
+
+}
+
+
